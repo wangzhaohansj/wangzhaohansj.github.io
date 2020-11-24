fe6d0805b3593d71f58a2b32719c16c257edf0cf
wangzhaohan <wangzhaohan@lareview.cf>
2020-11-24 22:16:13 +0800

    测试

--- assets/js/_commons/mima.js
+++ assets/js/_commons/mima.js
@@ -3,15 +3,15 @@ function password() {
 var testV = 1;
 var pass1 = prompt('请输入密码:688','');
 while (testV < 3000) {
-if (!pass1) 
+if (!pass1)
 history.go(-1);
 if (pass1 == "688") {
-alert('密码正确，准予进入！不过过路费还是要给的。2毛！');
+alert('密码正确');
 break;
 }
 testV+=1;
 var pass1 =
-prompt('密码不正确，你没获得主人的批准。');
+prompt('密码不正确');
 }
 if (pass1!="password" & testV ==3)
 history.go(-1);
