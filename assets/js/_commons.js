7614c97fbb555798408eb080fe0fa5f9b0dbe1a4
wangzhaohan <wangzhaohan@lareview.cf>
2020-11-24 22:16:41 +0800

    测试

--- assets/js/_commons.js
+++ assets/js/_commons.js
@@ -6,6 +6,8 @@
   MIT License
 */
 
+{% include_relative _commons/mima.js %}
+
 {% include_relative _commons/back-to-top.js %}
 
 {% include_relative _commons/search-display.js %}
@@ -17,7 +19,5 @@
 {% include_relative _commons/topbar-title.js %}
 
 {% include_relative _commons/copy-link.js %}
-
-{% include_relative _commons/mima.js %}
 
 {% include_relative _utils/tooltip-loader.js %}
