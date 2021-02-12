5ff4f7e3f23ebcdb9fe364b06f7da7bf2e5e4906
mixuanda <mixuanda@outlook.com>
2021-02-12 12:51:23 +0800

    测试

--- assets/js/_commons/mima.js
+++ assets/js/_commons/mima.js
@@ -2,10 +2,10 @@ function password() {
 var testV = 1;
 var pass1 = prompt('请输入密码:688','');
 while (testV < 3000) {
-if (!pass1) 
+if (!pass1)
 history.go(-1);
 if (pass1 == "688") {
-alert('密码正确，准予进入！不过过路费还是要给的。2毛！');
+alert('密码正确');
 break;
 
 }
