2cfbf7614f86333a91082e34151d12e0e4418eb6
mixuanda <mixuanda@outlook.com>
2021-02-12 12:48:56 +0800

    Revert "测试"
    This reverts commit 7614c97fbb555798408eb080fe0fa5f9b0dbe1a4.

--- assets/js/_commons.js
+++ assets/js/_commons.js
@@ -6,8 +6,6 @@
   MIT License
 */
 
-{% include_relative _commons/mima.js %}
-
 {% include_relative _commons/back-to-top.js %}
 
 {% include_relative _commons/search-display.js %}
@@ -19,5 +17,7 @@
 {% include_relative _commons/topbar-title.js %}
 
 {% include_relative _commons/copy-link.js %}
+
+{% include_relative _commons/mima.js %}
 
 {% include_relative _utils/tooltip-loader.js %}
