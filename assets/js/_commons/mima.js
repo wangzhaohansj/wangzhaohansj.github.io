d6b6ee3d1928ea38e35b5c0710ae9ed73076b2e8
mixuanda <mixuanda@outlook.com>
2021-02-12 12:55:09 +0800

    测试n+1

--- assets/js/_commons/mima.js
+++ assets/js/_commons/mima.js
@@ -1,17 +1,17 @@
 function password() {
 var testV = 1;
-var pass1 = prompt('请输入密码:688','');
+var pass1 = prompt('请输入密码:admin','');
 while (testV < 3000) {
 if (!pass1)
 history.go(-1);
-if (pass1 == "688") {
+if (pass1 == "shkl") {
 alert('密码正确');
 break;
 
 }
 testV+=1;
 var pass1 =
-prompt('密码不正确，你没获得主人的批准。');
+prompt('密码不正确');
 }
 if (pass1!="password" & testV ==3)
 history.go(-1);
